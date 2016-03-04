cba6339152203b8475854bd310a5116de4fcf01f
Greg Sypolt <gsypolt@gannett.com>
2016-03-03 22:08:41 -0500

    modified install.sh file

--- install.sh
+++ install.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-# Install Homebrew
+echo "-- Install Homebrew ---"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 
-# Install RVM
+echo "--- Install RVM ---"
 curl -sSL https://get.rvm.io | bash -s stable --ruby;
 source ~/.rvm/scripts/rvm;
+
+echo "--- Install all the dependencies for installing Ruby automatically on system using below command ---"
+rvm requirements;
 rvm list;
+
+echo "--- Install ruby version ---"
 rvm install 2.2.1;
+
+echo "--- Setup default ruby version ---"
 rvm --default use 2.2.1;
 
-# Install Bundler
+echo "--- Install bundler gem ---"
 gem install bundler;
